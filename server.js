2598a92f2bb3382ae1a8a2707dde4b499ba03197
superman76 <cbloodjr@gmail.com>
2016-02-16 15:45:30 -0700

    new function to change a bears name with put function

--- server.js
+++ server.js
@@ -61,6 +61,27 @@ router.route('/bears/:bear_id')
 			}
 		})
 	})
+	.put(function(req, res){
+		Bear.findById(req.params.bear_id, function(err, bear){
+		  if(err){
+		  	console.log(err)
+		  } else {
+		  	bear.name = req.body.name ? req.body.name : bear.name;
+		  	bear.age = req.body.age ? req.body.age : bear.age;
+		  	bear.gender = req.body.gender ? req.body.gender : bear.gender;
+		  	
+		  	bear.save(function(err){
+		  		if(err){
+		  			console.log(err)
+		  		} else {
+		  			res.json({title: "bear updated"});
+		  		}
+		  	})
+		  }
+		})
+	})
+
+
 
 
 app.use('/api', router);
