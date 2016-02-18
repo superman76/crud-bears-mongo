31ef69e359ff45a042683b32f08b04dbb54472a8
superman76 <cbloodjr@gmail.com>
2016-02-18 12:07:29 -0700

    bears.ejs submit & delete button

--- public/app.js
+++ public/app.js
@@ -1,5 +1,27 @@
 console.log('hello world');
 
+function deleteBear (event) {
+	event.preventDefault();
+
+	var $button = $(event.target)  
+
+	var id = $button.data("id");  
+	
+
+	console.log("Bear has been deleted: " + id)
+
+	$.ajax("/api/bears/" + id, {method: "DELETE"}).done(function() {
+		$button.closest('tr').remove();
+	});
+
+
+
+}
+
+$('.deleteBear').on('click', deleteBear);
+// This is an event subscription.  Anything with the class .deleteBear
+// when it is clicked
+
 
 
 
