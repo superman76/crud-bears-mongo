21d89ec26f4c67c36db3d07e88036a884e806570
superman76 <cbloodjr@gmail.com>
2016-02-18 13:42:02 -0700

    added addBear to app.js, alert is working

--- public/app.js
+++ public/app.js
@@ -13,11 +13,14 @@ function deleteBear (event) {
 	$.ajax("/api/bears/" + id, {method: "DELETE"}).done(function() {
 		$button.closest('tr').remove();
 	});
-
-
-
 }
 
+var addBear = function(event){
+	event.preventDefault();
+
+	alert('you clicked the add bear button');
+}
+$('#addBear').on('click', addBear);
 $('.deleteBear').on('click', deleteBear);
 // This is an event subscription.  Anything with the class .deleteBear
 // when it is clicked
